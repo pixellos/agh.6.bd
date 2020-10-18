536f628a55d7f9b81a2bec25c4c5f0203bca2579
Your Name <you@example.com>
2020-10-18 12:31:00 +0200

    Dodano Open-Api

--- src/main/java/com/agh/NorthwindApplication.java
+++ src/main/java/com/agh/NorthwindApplication.java
@@ -2,12 +2,33 @@ package com.agh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @SpringBootApplication
+@EnableSwagger2
+@Configuration
+@Import(SpringDataRestConfiguration.class)
 public class NorthwindApplication {
+
+	@Bean
+	public InternalResourceViewResolver defaultViewResolver() {
+		return new InternalResourceViewResolver();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(NorthwindApplication.class, args);
 	}
-
 }
