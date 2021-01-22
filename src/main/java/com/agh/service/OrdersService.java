4b013b3fd427a0901b73b9a5b326e496dc8a56c4
elochim-siemasz <michal.flak.96@gmail.com>
2021-01-22 21:36:03 +0100

    Bugfix - wasn't possible to create order

--- src/main/java/com/agh/service/OrdersService.java
+++ src/main/java/com/agh/service/OrdersService.java
@@ -10,6 +10,7 @@ import com.agh.util.DateConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -60,6 +61,7 @@ public class OrdersService {
         order.setShippers(shipper);
         order.setEmployees(employee);
         order.setCustomers(customer);
+        order.setOrderDate(LocalDate.now());
 
         order.setRequiredDate(DateConverter.toDate(request.getRequiredDate()));
         order.setShippedDate(DateConverter.toDate(request.getShippedDate()));
